503dda0e792d0ef3a5ec81957d0333e0df032752
javier <devjavicont@gmail.com>
2021-09-07 16:38:19 -0400

    la clase "is-active" del menu y boton del usuario ahora se eliminan al dar click en cualquier parte del documento

--- scripts.js
+++ scripts.js
@@ -5,11 +5,15 @@ function loadJS() {
     const userButton = document.getElementById("user-btn"),
           userMenu = document.getElementById("dd-menu")
     
-    userButton.addEventListener("click", () => {
+    userButton.addEventListener("click", e => {
+        e.stopPropagation()
         userButton.classList.toggle("is-active")
         userMenu.classList.toggle("is-active")
     })
-
-
     
+    
+    document.addEventListener("click", () => {
+        userButton.classList.remove("is-active")
+        userMenu.classList.remove("is-active")
+    })
 }
