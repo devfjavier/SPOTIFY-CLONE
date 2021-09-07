91a45e1fcbd0a89666caf1e34767dae4152a727f
javier <devjavicont@gmail.com>
2021-09-07 16:16:21 -0400

    dar click en el boton del usuario agrega la clase "is-active" si no lo tiene y lo quita si lo tiene

--- scripts.js
+++ scripts.js
@@ -2,7 +2,13 @@
 addEventListener("DOMContentLoaded", loadJS)
 
 function loadJS() {
-
+    const userButton = document.getElementById("user-btn"),
+          userMenu = document.getElementById("dd-menu")
+    
+    userButton.addEventListener("click", () => {
+        userButton.classList.toggle("is-active")
+        userMenu.classList.toggle("is-active")
+    })
 
 
     
