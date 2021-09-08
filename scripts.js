9df2abf0e81bcf107ba4e384db63cbc9a2dd3847
javier <devjavicont@gmail.com>
2021-09-08 13:21:05 -0400

    al dar click en el boton del item, se queda fijo y cambia de icono, y si esta fijo desaparece y cambia de icono

--- scripts.js
+++ scripts.js
@@ -2,9 +2,23 @@
 addEventListener("DOMContentLoaded", loadJS)
 
 function loadJS() {
-    const userButton = document.getElementById("user-btn"),
-          userMenu = document.getElementById("dd-menu")
+    const   userButton = document.getElementById("user-btn"),
+            userMenu = document.getElementById("dd-menu")
+
+    const   itemButton = Array.from(document.querySelectorAll(".item .play-pause"))
     
+    itemButton.map(button => {
+        button.addEventListener("click", e => {
+            button.classList.toggle("is-active")
+            
+            itemButton.map(btn => {
+                if (btn !== button) {
+                    btn.classList.remove("is-active")
+                }
+            })
+        })
+    })
+
     userButton.addEventListener("click", e => {
         e.stopPropagation()
         userButton.classList.toggle("is-active")
