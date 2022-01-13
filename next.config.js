5f5ed7a9a91858f79a4844f64918b2368b6fa733
javier <devjavicont@gmail.com>
2022-01-13 14:16:11 -0400

    Next config: se configura cdn en assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  basePath: "/Proyecto-de-Clon-de-Spotify"
+  basePath: "/Proyecto-de-Clon-de-Spotify",
+  assetPrefix: "https://cdn.statically.io/gh/devfjavier/Proyecto-de-Clon-de-Spotify/gh-pages/"
 }
