19342eff9d38a3a845ac2b4d4c6523e3964895a8
javier <devjavicont@gmail.com>
2022-01-20 18:56:49 -0400

    Panel: los links y los iconos cambian de color juntos en hover

--- components/core-sections/Panel.js
+++ components/core-sections/Panel.js
@@ -7,8 +7,8 @@ const Panel = () => {
       <ul className="menu">
         <li>
           <Link href="/">
-            <a className="link text fs-p4">
-              <Icon className="panel-icon" name="index" fill="#fff"/>
+            <a className="link fs-p4">
+              <Icon className="panel-icon" name="index" fill="currentColor"/>
               <span>Inicio</span>
             </a>
           </Link>
@@ -16,8 +16,8 @@ const Panel = () => {
 
         <li>
           <Link href="/search">
-            <a className="link text fs-p4">
-              <Icon className="panel-icon" name="search" stroke="#fff"/>
+            <a className="link fs-p4">
+              <Icon className="panel-icon" name="search" stroke="currentColor"/>
               <span>Buscar</span>
             </a>
           </Link>
@@ -25,8 +25,8 @@ const Panel = () => {
 
         <li className="menu-link">
           <Link href="/collection/playlists">
-            <a className="link text fs-p4">
-              <Icon className="panel-icon" name="library" stroke="#fff"/>
+            <a className="link fs-p4">
+              <Icon className="panel-icon" name="library" stroke="currentColor"/>
               <span>Tu Biblioteca</span>
             </a>
           </Link>
@@ -34,28 +34,28 @@ const Panel = () => {
 
         <li className="footer-menu-link">
           <Link href="/collection/tracks">
-            <a className="link text fs-p4">
-              <Icon className="panel-icon" name="tracks2" fill="#fff"/>
+            <a className="link fs-p4">
+              <Icon className="panel-icon" name="tracks2" fill="currentColor"/>
               <span>Favoritos</span>
             </a>
           </Link>
         </li>
 
         <li className="footer-menu-link">
-          <a className="link text fs-p4" href="#">
-            <Icon className="panel-icon" name="spotify" fill="#fff"/>
+          <a className="link fs-p4" href="#">
+            <Icon className="panel-icon" name="spotify" fill="currentColor"/>
               <span>Premium</span>
           </a>
         </li>
       </ul>
     
       <div className="buttons">
-        <button className="link create-playlist text fs-p4">
+        <button className="link create-playlist fs-p4">
           <Icon className="panel-icon" name="createPlaylist"/>
           <span>Crear playlist</span>
         </button>
         <Link href="/collection/tracks">
-          <a className="link text fs-p4">
+          <a className="link fs-p4">
             <Icon className="panel-icon" name="tracks"/>
             <span>Tus me gusta</span>
           </a>
