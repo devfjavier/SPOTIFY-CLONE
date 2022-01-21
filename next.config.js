6e7a65a94480adabc54a17be14191d9238cb7779
javier <devjavicont@gmail.com>
2022-01-21 14:21:03 -0400

    Next config: basePath y assetPrefix solo se aplican en "next build"

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-  reactStrictMode: true,
-  basePath: "/Proyecto-de-Clon-de-Spotify",
-  assetPrefix: "https://cdn.statically.io/gh/devfjavier/Proyecto-de-Clon-de-Spotify/gh-pages/"
+const { PHASE_PRODUCTION_BUILD } = require("next/constants")
+
+module.exports = phase => {
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    return {
+      reactStrictMode: true,
+      basePath: "/Proyecto-de-Clon-de-Spotify",
+      assetPrefix: "https://cdn.statically.io/gh/devfjavier/Proyecto-de-Clon-de-Spotify/gh-pages/"
+    }
+  } 
+  else {
+    return {
+      reactStrictMode: true
+    }  
+  }
 }
