c5194d5ca721816a8f3dc9ea8d02fafe221dc6e7
javier <devjavicont@gmail.com>
2022-04-29 18:59:21 -0400

    Track: corrige contenido

--- pages/track/track.js
+++ pages/track/track.js
@@ -1,15 +1,11 @@
 import RootContainer from "../../components/fragments/RootContainer"
 import { NORMAL_HEADER } from "../../consts/header_names"
-import Header from "../../components/core-sections/Header"
-import Panel from "../../components/core-sections/panel"
 
 const Track = () => {
   return (
-    <RootContainer pageClassName="track">
-      <Panel/>
-      <Header headerName={NORMAL_HEADER}/>
+    <RootContainer pageClassName="track" headerName={NORMAL_HEADER}>
       <main>
-
+        dynamic route
       </main>
     </RootContainer>
   )
