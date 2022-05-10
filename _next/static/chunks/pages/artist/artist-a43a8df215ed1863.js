7e246426bcbb632d5abf6adc505b4887b3f8b11b
javier <devjavicont@gmail.com>
2022-05-10 17:19:25 -0400

    Updates

--- _next/static/chunks/pages/artist/artist-a43a8df215ed1863.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[411],{1902:function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/artist/artist",function(){return u(6311)}])},6311:function(n,t,u){"use strict";u.r(t);t.default=function(){return"ARTISTA"}}},function(n){n.O(0,[774,888,179],(function(){return t=1902,n(n.s=t);var t}));var t=n.O();_N_E=t}]);
