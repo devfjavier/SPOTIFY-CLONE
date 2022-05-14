c57aca6c99295e9e515b0e35fc8b860e30b1f79e
javier <devjavicont@gmail.com>
2022-05-14 14:29:03 -0400

    RootContainer: separa estilos de escritorio y movil

--- components/fragments/RootContainer.js
+++ components/fragments/RootContainer.js
@@ -1,20 +1,23 @@
+import { root_uiM, root_uiDesk } from "../../styles/config/root-layout.module.scss"
 import NextHead from "next/head"
 import Footer from "../core-sections/Footer"
 import Header from "../core-sections/header"
 import Panel from "../core-sections/panel"
 
 const RootContainer = ({ panelName, headerName, collectionPageName, children }) => {
-  return <>
-    <NextHead>
-      <title>SPOTIFY CLONE</title>
-      <meta name="description" content="Spotify clone" />
-      <link rel="icon" href={process.env.projectPath + "/svgs/spotify.svg"} />
-    </NextHead>
-    <Panel name={panelName} />
-    <Header name={headerName} collectionPageName={collectionPageName} />
-    {children}
-    <Footer />
-  </>
+  return (
+    <div id={process.env.uiM ? root_uiM : root_uiDesk}>
+      <NextHead>
+        <title>SPOTIFY CLONE</title>
+        <meta name="description" content="Spotify clone" />
+        <link rel="icon" href={process.env.projectPath + "/svgs/spotify.svg"} />
+      </NextHead>
+      <Panel name={panelName} />
+      <Header name={headerName} collectionPageName={collectionPageName} />
+      {children}
+      <Footer />
+    </div>
+  )
 }
 
 export default RootContainer
