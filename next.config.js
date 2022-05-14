3bdcc15c357c3590bab1683bf49898f1afb56d39
javier <devjavicont@gmail.com>
2022-05-14 14:34:00 -0400

    next.config.js: corrige la configuracion para escritorio

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
-// const { PHASE_PRODUCTION_BUILD } = require("next/constants")
+const { PHASE_PRODUCTION_BUILD } = require("next/constants")
 
-module.exports = _phase => {
+module.exports = phase => {
   const isActive_uiM = true //change to false to build the desk ui, cache must be disabled
   let PROJECT_PATH = ""
   let config = {}
 
-  if (isActive_uiM === false) {
+  if (phase === PHASE_PRODUCTION_BUILD && isActive_uiM === false) {
     PROJECT_PATH = "/SPOTIFY-CLONE"
 
     config = {
       basePath: PROJECT_PATH,
       assetPrefix: `https://cdn.statically.io/gh/devfjavier${PROJECT_PATH}/gh-pages/`,
       async redirects() {
-        if (isActive_uiM) return []
-
         return [
           {
             source: "/library",
