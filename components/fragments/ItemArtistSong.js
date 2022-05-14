179d74ca28c9680f61764b161954498f96123d4c
javier <devjavicont@gmail.com>
2022-05-14 15:40:54 -0400

    ItemArtistsSong: separa clases para escritorio y movil

--- components/fragments/ItemArtistSong.js
+++ components/fragments/ItemArtistSong.js
@@ -1,11 +1,13 @@
 import classes from "../../styles/fragments/item-artist-song.module.scss"
-import { item } from "../../styles/fragments/btn-play-pause.module.scss"
+import { item as btnItem } from "../../styles/fragments/btn-play-pause.module.scss"
 import BtnPlayPause from "./BtnPlayPause"
 
+const is_uiM = process.env.uiM
+
 const ItemArtistSong = ({ compact = false, artist = false }) => {
-  const is_uiM = process.env.uiM
+
   return (
-    <ItemBase className={compact ? classes.itemCompact : "" + (is_uiM ? ` ${classes.s_uiM}` : "")}>
+    <Base className={compact ? ` ${classes.itemCompact}` : ""}>
       <div className={classes.container}>
         <img className={artist && !compact ? "h-b-radius-50" : null} src={process.env.projectPath + "/img/song.webp"} alt="godot engine" />
         {compact || is_uiM || <BtnPlayPause item />}
@@ -15,13 +17,15 @@ const ItemArtistSong = ({ compact = false, artist = false }) => {
         <h3 className="h-color-light">title</h3>
         {compact || <p className="h-color-gray">subtitle</p>}
       </div>
-    </ItemBase>
+    </Base>
   )
 }
 
-function ItemBase({ className, children }) {
+function Base({ className, children }) {
+  const class_uiM = classes[is_uiM ? "s_uiM" : "s_uiDesk"]
+
   return (
-    <article className={`${item} ${classes.item}` + (className ? ` ${className}` : "")}>
+    <article className={`${btnItem} ${classes.item} ${class_uiM}${className}`}>
       <a className={classes.sourceLink} href="#"></a>
       {children}
     </article>
