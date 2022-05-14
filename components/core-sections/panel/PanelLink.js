e2ab9684e5b7f3bfaaecfc2d606e90fb25fd0b36
javier <devjavicont@gmail.com>
2022-05-14 14:27:27 -0400

    PanelLink: agrega estilos para escritorio y movil

--- components/core-sections/panel/PanelLink.js
+++ components/core-sections/panel/PanelLink.js
@@ -3,11 +3,12 @@ import Icon from "../../icon-svg"
 import classes from "../../../styles/core-sections/panel/panel-item.module.scss"
 
 const PanelLink = ({ children, href, iconName, className }) => {
+  const class_uiM =  ` ${ classes[process.env.uiM ? "s_uiM" : "s_uiDesk"]}`
   const customClassName = className ? ` ${className}` : ""
   return (
     <li>
       <Link href={href}>
-        <a className={`h-color-light h-fs-st4 ${classes.item}${customClassName}`}>
+        <a className={`h-color-light h-fs-st4 ${classes.item}${class_uiM}${customClassName}`}>
           {iconName && <Icon name={iconName} fill="currentColor" size="1.4rem" />}
           {children}
         </a>
