39d25dcd08285a32c942f538c5c651543b11c71c
javier <devjavicont@gmail.com>
2022-05-13 20:35:43 -0400

    next.config.js: mejora la configuracion

--- next.config.js
+++ next.config.js
@@ -1,16 +1,27 @@
-const { PHASE_PRODUCTION_BUILD } = require("next/constants")
+// const { PHASE_PRODUCTION_BUILD } = require("next/constants")
 
-module.exports = phase => {
+module.exports = _phase => {
   const isActive_uiM = true //change to false to build the desk ui, cache must be disabled
   let PROJECT_PATH = ""
   let config = {}
 
-  if (phase === PHASE_PRODUCTION_BUILD && !isActive_uiM) {
+  if (isActive_uiM === false) {
     PROJECT_PATH = "/SPOTIFY-CLONE"
 
     config = {
       basePath: PROJECT_PATH,
-      assetPrefix: `https://cdn.statically.io/gh/devfjavier${PROJECT_PATH}/gh-pages/`
+      assetPrefix: `https://cdn.statically.io/gh/devfjavier${PROJECT_PATH}/gh-pages/`,
+      async redirects() {
+        if (isActive_uiM) return []
+
+        return [
+          {
+            source: "/library",
+            destination: "/library/playlists",
+            permanent: false
+          }
+        ]
+      }
     }
   }
 
@@ -19,17 +30,6 @@ module.exports = phase => {
     env: {
       projectPath: PROJECT_PATH,
       uiM: isActive_uiM
-    },
-    async redirects() {
-      if (isActive_uiM) return []
-
-      return [
-        {
-          source: "/collection",
-          destination: "/collection/playlists",
-          permanent: false
-        }
-      ]
     },
     ...config
   }
