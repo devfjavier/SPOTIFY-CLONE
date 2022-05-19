1a767fb3d79f8de3d5ad1a98929f9bd34458927e
javier <devjavicont@gmail.com>
2022-05-19 17:49:48 -0400

    BtnPlayPause aparece en ItemArtistSong compact para escritorio

--- components/fragments/BtnPlayPause.js
+++ components/fragments/BtnPlayPause.js
@@ -2,16 +2,18 @@ import { useRef } from "react"
 import classes from "../../styles/fragments/btn-play-pause.module.scss"
 import Icon from "../icon-svg"
 
-const BtnPlayPause = ({ item = false }) => {
+const is_uiM = process.env.uiM
+
+const BtnPlayPause = ({ itemCompact = false }) => {
   const buttonRef = useRef()
   const containerRef = useRef()
 
   const toggleButton = () => {
     buttonRef.current.classList.toggle(classes.active)
-    if (item) containerRef.current.classList.toggle(classes.active)
+    if (!is_uiM) containerRef.current.classList.toggle(classes.active)
   }
   return (
-    <div className={item ? classes.container : null} ref={containerRef}>
+    <div className={`${classes.container} ${classes[itemCompact ? "compact" : "card"]}`} ref={containerRef}>
       <button className={"h-unset " + classes.btn} ref={buttonRef} onClick={toggleButton}>
         <Icon className={classes.playIcon} name="play" fill="#000" size="1.5rem" />
         <Icon className={classes.pauseIcon} name="pause" fill="#000" size="1.5rem" />
--- components/fragments/ItemArtistSong.js
+++ components/fragments/ItemArtistSong.js
@@ -5,17 +5,28 @@ import BtnPlayPause from "./BtnPlayPause"
 const is_uiM = process.env.uiM
 
 const ItemArtistSong = ({ compact = false, artist = false }) => {
+  const img = <img className={artist && !compact ? "h-b-radius-50" : null} src={process.env.projectPath + "/img/song.webp"} alt="godot engine" />
+
+  if (compact) {
+    return (
+      <Base className={classes.itemCompact}>
+        {img}
+        <h3 className="h-color-light">title</h3>
+        {is_uiM ? null : <BtnPlayPause itemCompact />}
+      </Base>
+    )
+  }
 
   return (
-    <Base className={compact ? ` ${classes.itemCompact}` : ""}>
-      <div className={classes.container}>
-        <img className={artist && !compact ? "h-b-radius-50" : null} src={process.env.projectPath + "/img/song.webp"} alt="godot engine" />
-        {compact || is_uiM || <BtnPlayPause item />}
+    <Base className={classes.itemCard}>
+      <div className={classes.imgContainer}>
+        {img}
+        {is_uiM ? null : <BtnPlayPause />}
       </div>
 
-      <div className={compact ? null : classes.infoContainer}>
+      <div className={classes.infoContainer + (is_uiM ? "" : ` ${classes.s_uiDesk}`)}>
         <h3 className="h-color-light">title</h3>
-        {compact || <p className="h-color-gray">subtitle</p>}
+        <p className="h-color-gray">subtitle</p>
       </div>
     </Base>
   )
@@ -25,7 +36,7 @@ function Base({ className, children }) {
   const class_uiM = classes[is_uiM ? "s_uiM" : "s_uiDesk"]
 
   return (
-    <article className={`h-bg-color-dark-4 ${btnItem} ${classes.item} ${class_uiM}${className}`}>
+    <article className={`h-bg-color-dark-4 ${btnItem} ${classes.base} ${className} ${class_uiM}`}>
       <a className={classes.sourceLink} href="#"></a>
       {children}
     </article>
